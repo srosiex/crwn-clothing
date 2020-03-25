8e01695a449c4bf1de9586f678cd4834ab75c014
Sarah Henderson <sarahrose.henderson@gmail.com>
2020-03-25 10:59:20 -0500

    update menu-times to nav withRouter HOC

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-serviceWorker.unregister();
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+<BrowserRouter>
+    <App />
+</BrowserRouter>
+
+, document.getElementById('root'));
+
